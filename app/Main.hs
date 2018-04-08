db2920087ba839f3691870b7c8fc0cab6fcc304e
Austin Moore <mooreat2@miamioh.edu>
2018-04-08 18:55:44 -0400

    I'm not sure what this adds

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import Lib
+import Graphics.Gloss
+import Graphics.Gloss.Data.Display
 
 main :: IO ()
-main = putStrLn "Dank Memes"
+main = display (InWindow "Nice Window" (400, 400) (10, 10)) white (cpuToPicture defaultCpu)
